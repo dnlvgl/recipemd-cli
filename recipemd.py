fefeae8376f404d4b5f4ac41861e2095e60e168c
Daniel Vogel <daniel@ffd8ffe0.com>
2014-10-01 20:45:28 +0200

    fixed encoding

--- recipemd.py
+++ recipemd.py
@@ -1,5 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from bs4 import BeautifulSoup
 import urllib
+import codecs
 
 # implement argparse
 
@@ -8,28 +12,25 @@ page = urllib.urlopen(url)
 soup = BeautifulSoup(page.read())
 
 #title
-title_ws = soup.find('h1', attrs={'class':'page-title'}).text
-title = title_ws.replace(' ', '') # remove whitespace
+title = soup.find('h1', attrs={'class':'page-title'}).text
 
 # ingredients
-data = []
+ingreds = []
 table = soup.find('table', attrs={'class':'incredients'})
 rows = table.find_all('tr')
 for row in rows:
-    cols = row.find_all('td')
-    cols = [ele.text.strip() for ele in cols]
-    data.append([ele for ele in cols if ele]) # get rid of empty values
-
+	cols = row.find_all('td')
+	cols = [ele.text.strip() for ele in cols]
+	ingreds.append([ele for ele in cols if ele]) # get rid of empty values
 # instructions
-instructions = soup.find('div', attrs={'id':'rezept-zubereitung'}).text # only get text
-
+instruct = soup.find('div', attrs={'id':'rezept-zubereitung'}).text # only get text
 # write to file
-with open(title.lower() + '.md', 'w') as f: # use title of site as name for file
-    f.write('# ' + title  + u'\n' + u'\n')
-    f.write('## Zutaten' + u'\n' + u'\n')
-    for inner_list in data:
-        f.write('- ' + ' '.join(inner_list).encode('utf-8') + '\n')
-        # data = list of lists; join inner lists
-    f.write(u'\n' + u'\n' + '## Zubereitung' + u'\n' + u'\n')
-    f.write((instructions.strip()).encode('utf-8')) #.strip() to remove leadin and ending whitespace
-    f.write('\n\n[Quelle](' + url + ')')
+with codecs.open(title.lower() + '.md', 'w', encoding="utf-8") as f: # use title of site as name for file
+	f.write('# ' + title + '\n\n')
+	f.write('## Zutaten' + '\n\n')
+	for inner_list in ingreds:
+	    f.write('- ' + ' '.join(inner_list) + '\n')
+	    # data = list of lists; join inner lists
+	f.write('\n\n' + '## Zubereitung' + '\n\n')
+	f.write(instruct.strip()) #.strip() to remove leadin and ending whitespace
+
