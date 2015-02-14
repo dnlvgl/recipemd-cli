f8023d1235435d6e36f7dc5a5aec228f89762b64
Daniel Vogel <daniel@ffd8ffe0.com>
2015-02-14 16:21:48 +0100

    python3 compatibility

--- recipemd.py
+++ recipemd.py
@@ -2,14 +2,14 @@
 # -*- coding: utf-8 -*-
 
 from bs4 import BeautifulSoup
-import urllib
+import requests
 import codecs
 import sys
 
 
 def chefkoch(soup):
     # title
-    title = soup.find('h1', attrs={'class': 'page-title'}).text.replace(' ', '')
+    title = soup.find('h1', attrs={'class': 'page-title'}).text
     # ingredients
     ingreds = []
     table = soup.find('table', attrs={'class': 'incredients'})
@@ -17,27 +17,30 @@ def chefkoch(soup):
     for row in rows:
         cols = row.find_all('td')
         cols = [s.text.strip() for s in cols]
-        ingreds.append([ele for ele in cols if ele]) # get rid of empty values
+        ingreds.append([ele for ele in cols if ele])  # get rid of empty values
     ingreds = ['- ' + ' '.join(s) for s in ingreds]
     # instructions
     instruct = soup.find('div', attrs={'id': 'rezept-zubereitung'}).text  # only get text
-    instruct = instruct.strip() # remove leadin and ending whitespace
+    instruct = instruct.strip()  # remove leadin and ending whitespace
     # write to file
     writeFile(title, ingreds, instruct)
 
+
 def allrecipes(soup):
     # title
-    title = soup.find('h1', attrs={'id':'itemTitle'}).text
+    title = soup.find('h1', attrs={'id': 'itemTitle'}).text
     # ingredients
     ingreds = soup.find('div', attrs={'class': 'ingred-left'})
     ingreds = [s.getText().strip() for s in ingreds.findAll('li')]
-    ingreds = ['- ' + s.replace('\n', ' ') for s in ingreds] #add dash + remove newlines
+    ingreds = ['- ' + s.replace('\n', ' ') for s in ingreds]  # add dash + remove newlines
+    ingreds = [" ".join(s.split()) for s in ingreds]  # remove whitespace
     # instructions
-    instruct = soup.find('div', attrs={'class':'directLeft'})
+    instruct = soup.find('div', attrs={'class': 'directLeft'})
     instruct = [s.getText().strip() for s in instruct.findAll('li')]
     instruct = ' '.join(instruct)
     # write to file
     writeFile(title, ingreds, instruct)
+
 
 def writeFile(title, ingreds, instruct):
         with codecs.open(title.lower().replace(' ', '-') + '.md', 'w', encoding="utf-8") as f:
@@ -50,15 +53,15 @@ def writeFile(title, ingreds, instruct):
 
 def main():
     url = sys.argv[1]
-    page = urllib.urlopen(url)
-    soup = BeautifulSoup(page.read())
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, "html5lib")
 
     if url.startswith('http://www.chefkoch.de/'):
         chefkoch(soup)
     elif url.startswith('http://allrecipes.com/'):
         allrecipes(soup)
     else:
-        print 'nope'
+        print ('nope')
 
 
 if __name__ == "__main__":
